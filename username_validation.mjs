53babd7ab1d0fe31e9e919649b75d3e03eb3ab47
MrFasolo97 <79fasolo97@gmail.com>
2023-06-21 11:37:01 +0200

    Some style changes

--- username_validation.mjs
+++ username_validation.mjs
@@ -4,18 +4,18 @@ function validateUsername(value, maxLength, minLength, allowedChars, allowedChar
     if (!minLength)
         minLength = 0
     if (typeof value !== 'string')
-        return "The username should be a string!"
+        return "<span style='color: red;'>The username should be a string!</span>"
     if (value.length > maxLength)
-        return "Username too long!"
+        return "<span style='color: red;'>Username too long!</span>"
     if (value.length < minLength)
-        return "Username too short!"
+        return "<span style='color: red;'>Username too short!</span>"
     if (allowedChars)
         for (let i = 0; i < value.length; i++)
             if (allowedChars.indexOf(value[i]) === -1)
                 if (i === 0 || i === value.length-1 && allowedCharsMiddle && allowedCharsMiddle.indexOf(value[i]) !== -1)
-                    return ". and - characters are allowed only between the name, not at the beginning nor at the end."
+                    return "<span style='color: red;'>. and - characters are allowed only between the name, not at the beginning nor at the end.</span>"
                 else if(allowedCharsMiddle && allowedCharsMiddle.indexOf(value[i]) === -1)
-                    return "That username includes forbidden character(s)! For example \""+value[i]+'"';
+                    return "<span style='color: red;'>That username includes forbidden character(s)! For example \""+value[i]+'"</span>';
     return true
 }
 
